b53bae2ca8159ec8b0440a2677e252195fa676fd
haleyjung <73789849+haleyjung@users.noreply.github.com>
2020-12-04 23:00:00 -0500

    initial commit

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = function handleListening() {
 	console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+function handleHome(req, res) {
+	res.send("Hello from home!");
 }
+
+function handleProfile(req, res) {
+	res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
